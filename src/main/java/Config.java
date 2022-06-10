c15b6912f2ad4c04692a9acc0ed46264822b5f78
xiangwei <teamolp326@gmail.com>
2022-06-10 13:27:05 +0800

    Initial commit

---
+++ src/main/java/Config.java
@@ -0,0 +1,26 @@
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class Config {
+
+    private static final Properties properties = new Properties();
+    private static boolean propertiesInitialized = false;
+
+    public static String get(String propertyName) {
+        if (!propertiesInitialized) {
+            try (InputStream inputStream = new FileInputStream("SwiftDecoder.Properties")) {
+                properties.load(inputStream);
+                propertiesInitialized = true;
+            } catch (Exception e) {
+                System.out.println("Trying to load config " + propertyName + " failed");
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return properties.getOrDefault(propertyName, "").toString();
+    }
+
+}
